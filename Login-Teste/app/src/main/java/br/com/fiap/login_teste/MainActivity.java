6a70e5d0ac5c50c31e064a0eb41e3ad0db01295d
Carlos Henrique Umiji Dias <carlos.dias@MJV.net>
2018-08-28 14:30:34 -0300

    terminado projeto teste-login

--- Login-Teste/app/src/main/java/br/com/fiap/login_teste/MainActivity.java
+++ Login-Teste/app/src/main/java/br/com/fiap/login_teste/MainActivity.java
@@ -26,24 +26,53 @@ public class MainActivity extends AppCompatActivity {
 
         edtUsuario = findViewById(R.id.edtUsuario);
         edtSenha = findViewById(R.id.edtSenha);
+        chkSalvar = findViewById(R.id.chkSalvar);
+
+        sp = getSharedPreferences("login", MODE_PRIVATE);
+
+        boolean salvar = sp.getBoolean("salvar",false);
+        String usuario = sp.getString("usuario", null);
+        String senha = sp.getString("senha",null);
+
+        getSupportActionBar().setTitle("lOGIN");
+
+        chkSalvar.setChecked(salvar);
+        edtUsuario.setText(usuario);
+        edtSenha.setText(senha);
 
     }
 
     public void logar(View view) {
 
         String nome =  edtUsuario.getText().toString();
-        String senha = edtSenha.getText().toString();
+        String vsenha = edtSenha.getText().toString();
 
-        if(nome.equals("FIAP") && senha.equals("123456") ){
+        if(nome.equalsIgnoreCase("FIAP") && vsenha.equals("123456") ){
+            SharedPreferences.Editor editor =  sp.edit();
+
+            if(chkSalvar.isChecked()){
+                boolean salvar = chkSalvar.isChecked();
+                String usuario = edtUsuario.getText().toString();
+                String senha = edtSenha.getText().toString();
+
+                editor.putBoolean("salvar",true);
+                editor.putString("usuario", usuario);
+                editor.putString("senha", senha);
+                editor.commit();
+
+            }else{
+                editor.putBoolean("salvar",false);
+                editor.putString("usuario", null);
+                editor.putString("senha",null);
+                editor.commit();
+            }
+
             Toast.makeText(this, R.string.sucesso, Toast.LENGTH_SHORT).show();
             Intent it = new Intent(this ,PreferenciasActivity.class);
             startActivity(it);
 
         }else {
             Toast.makeText(this, R.string.errou, Toast.LENGTH_SHORT).show();
-            Toast.makeText(this, edtUsuario.getText().toString(), Toast.LENGTH_SHORT).show();
-            Toast.makeText(this, edtSenha.getText().toString(), Toast.LENGTH_SHORT).show();
         }
-
     }
 }
--- Login-Teste/app/src/main/java/br/com/fiap/login_teste/PreferenciasActivity.java
+++ Login-Teste/app/src/main/java/br/com/fiap/login_teste/PreferenciasActivity.java
@@ -1,13 +1,96 @@
 package br.com.fiap.login_teste;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 
 public class PreferenciasActivity extends AppCompatActivity {
+
+    RadioButton rbVermelho;
+    RadioButton rbAzul;
+    RadioButton rbVerde;
+
+    LinearLayout llPreferencias;
+
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferencias);
+
+        rbVermelho = findViewById(R.id.rbVermelho);
+        rbAzul = findViewById(R.id.rbAzul);
+        rbVerde = findViewById(R.id.rbVerde);
+        llPreferencias = findViewById(R.id.llPreferencias);
+
+        getSupportActionBar().setTitle("Preferências");
+
+        sp = getSharedPreferences("bgColor", MODE_PRIVATE);
+        String color = sp.getString("color", "");
+
+        String cor = color.toString();
+
+        if(cor.equalsIgnoreCase("RED"))
+        {
+            llPreferencias.setBackgroundColor(Color.RED);
+        }
+
+        if(cor.equalsIgnoreCase("GREEN"))
+        {
+            llPreferencias.setBackgroundColor(Color.GREEN);
+        }
+
+        if(cor.equalsIgnoreCase("BLUE"))
+        {
+            llPreferencias.setBackgroundColor(Color.BLUE);
+        }
+    }
+
+    public void Salvar(View view) {
+
+        SharedPreferences.Editor editor = sp.edit();
+
+        if(rbVermelho.isChecked())
+        {
+            String cor = "RED";
+            editor.putString("color", cor);
+            editor.commit();
+            llPreferencias.setBackgroundColor(Color.RED);
+        }
+
+        if(rbVerde.isChecked())
+        {
+            String cor = "GREEN";
+            editor.putString("color", cor);
+            editor.commit();
+            llPreferencias.setBackgroundColor(Color.GREEN);
+        }
+
+        if(rbAzul.isChecked())
+        {
+            String cor = "BLUE";
+            editor.putString("color", cor);
+            editor.commit();
+            llPreferencias.setBackgroundColor(Color.BLUE);
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
